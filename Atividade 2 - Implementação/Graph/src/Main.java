08d96bfac162860232f1090fff55659ad6742b2b
Fabio Lucas <fabiolucs19@gmail.com>
2022-09-16 20:28:34 -0300

    updating

--- Atividade 2 - Implementação/Graph/src/Main.java
+++ Atividade 2 - Implementação/Graph/src/Main.java
@@ -4,19 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Atividade 2
+        // Atividade 2 - Lista de adjacência , ordem , grau , vértice isolado e
+        // extremidade
 
         Graph g = new Graph("Graph/src/pequenoG.txt");
-        g.printListAdj();
+        g.printListAdj();   // Printa as informações referente ao grafo dado o txt acima
 
-        // Atividade 3
-        
+        // Atividade 3 - BFS - Busca em Largura
+
         BreadthSearch bfs = new BreadthSearch(g);
-        bfs.startBFS(0);
+        bfs.startBFS(0);  // Insere o número do vértice de início
 
 
+        // Atividade 3 - Imprimi o menor caminho dado 2 vértices
+        int start_vertex = 0;
+        int destiny_vertex = 4;
 
-       
+        System.out.println("O menor caminho entre " + start_vertex + " e " + destiny_vertex);
+        bfs.find_the_shortest_way(start_vertex, destiny_vertex);
 
     }
 
